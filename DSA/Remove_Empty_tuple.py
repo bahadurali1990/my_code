19f4e80a83716ff423c0d8ef5665ce51c98d2d71
bahadurali90 <bahadurali0@gmail.com>
2026-01-28 07:49:53 -0800

    done

--- DSA/Remove_Empty_tuple.py
+++ DSA/Remove_Empty_tuple.py
@@ -10,10 +10,26 @@ print(output)
 
 # using list comprehension
 
-tup = [(1,2,3),(),(1,2),(),(1,2,3,4)]
+"""tup = [(1,2,3),(),(1,2),(),(1,2,3,4)]
 
 output = [x for x in tup if len(x)>0]
 
 print(output)
+"""
+
+"""d = {'gfg' : [5,6,7,8],' is' : [10,11,7,5], 'best' : [6,12,10,8], 'for' : [1,2,5]}
+final_out = list(set(sum(d.values(),[])))
+
+print(final_out)
+"""
+
+"""dict1 = {"a":10,"b":20,"c":30,"d":40,"e":50}
+
+total_sum = sum(dict1.values())
+
+print(total_sum)
+"""
+
+
 
 
