70c3cbca67f66837c78c6549bb07107c996393bc
bahadurali90 <bahadurali0@gmail.com>
2026-01-28 07:10:31 -0800

    done

--- DSA/Remove_Empty_tuple.py
+++ DSA/Remove_Empty_tuple.py
@@ -1,12 +1,19 @@
 # using filter function
 
-tup = [(1,2,3),(),(1,2),(),(1,2,3,4)]
+"""tup = [(1,2,3),(),(1,2),(),(1,2,3,4)]
 
 output=  list(filter(lambda x : len(x)>0, tup))
 
 print(tup)
 print(output)
+"""
 
+# using list comprehension
 
+tup = [(1,2,3),(),(1,2),(),(1,2,3,4)]
+
+output = [x for x in tup if len(x)>0]
+
+print(output)
 
 
