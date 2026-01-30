f4de8aad8e3ea809242e4e8ee57b70050fa3261c
bahadurali90 <bahadurali0@gmail.com>
2026-01-29 18:26:22 -0800

    done

--- Scikit-learn/Pratice_exercise_1.py
+++ Scikit-learn/Pratice_exercise_1.py
@@ -1,6 +1,6 @@
 ##---------------------------------------KNN based imputers-------------------------------------------
 
-from sklearn.impute import KNNImputer
+"""from sklearn.impute import KNNImputer
 import numpy as np
 
 X = [
@@ -15,3 +15,25 @@ X_new = imputer.fit_transform(X)
 
 print(X)
 print(X_new)
+"""
+##------------------------------------Creation of 1D datasets with some noise --------------------------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+rng = np.random.RandomState(42)
+
+X = np.sort(5*rng.rand(80,1),axis=0)
+
+y = np.sin(X).ravel()
+
+fig , axes = plt.subplots(1,2)
+axes[0].plot(X,y)
+
+y[::5] = y[::5]+3*(0.5-rng.rand(16))
+
+axes[1].plot(X,y)
+
+plt.show()
+
+
