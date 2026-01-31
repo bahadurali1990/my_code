690e1737ee3e55b25c0a92a080f93ec608e0182a
bahadurali90 <bahadurali0@gmail.com>
2026-01-30 21:22:31 -0800

    done

--- PyTorch_Code_Examples/Dataloader.py
+++ PyTorch_Code_Examples/Dataloader.py
@@ -1,4 +1,4 @@
-import numpy as np
+"""import numpy as np
 import torch
 
 from torch.utils.data import dataset
@@ -19,6 +19,42 @@ class TensorCustomData(dataset):
     def __len__(self):
 
         return self.n_samples
+"""
+##-----------------------------------Examples of some In-build datasets-------------------------------------
+import torch
+import matplotlib.pyplot as plt
+from torchvision.datasets import Caltech101
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+
+transform = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,))])
+
+datasets  = Caltech101(root='/Users/ahad/MNIST/Image_dataset1',transform=transform)
+
+train_size =  int(0.8*len(datasets))
+test_size  =  int(len(datasets)-train_size)
+
+train_datasets , test_datasets = random_split(datasets,[train_size,test_size])
+
+train_loader = DataLoader(train_datasets,shuffle=True,batch_size=32)
+test_loader  = DataLoader(test_datasets,shuffle=True,batch_size=32)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
