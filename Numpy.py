7c5da2ea91887e2e61929f350fdc93d595877cf8
bahadurali90 <bahadurali0@gmail.com>
2026-02-04 09:00:29 -0800

    done

--- Numpy.py
+++ Numpy.py
@@ -1,4 +1,4 @@
-# Numpy Broadcasting Exercise in Python
+""""# Numpy Broadcasting Exercise in Python
 import numpy as np
 
 A = np.array([1,
@@ -15,9 +15,9 @@ print(B_Shape)
 
 C=A+B
 print(C)
-
+"""
 #-------------------------------------------------------Polynomial-------------------------------------------
-
+"""
 import numpy as np
 
 from numpy.polynomial import Polynomial
@@ -25,3 +25,28 @@ from numpy.polynomial import Polynomial
 p = Polynomial([1,2,3,4,5,6])
 
 print(p(2))
+"""
+
+# ------------------------------------------Numpy Copyto functions -----------------------------------------
+
+"""import numpy as np
+
+A = np.array([1,2,3,4,5,6])
+B = np.zeros(6)
+np.copyto(B,A,casting='unsafe')
+
+print(A)
+"""
+
+#-------------------------------------- Find the shape and dimentions of Numpy array ----------------------
+
+"""import numpy  as np
+
+A = np.array([[1,2,3,4,5,6],[6,8,9,6,8,9]])
+print(A.ndim)
+print(A.shape)
+"""
+
+#
+
+
